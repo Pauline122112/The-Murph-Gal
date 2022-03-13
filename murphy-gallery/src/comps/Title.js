71f4cd7b0ad820cb8cd230fe3bb8d637b528f88f
Pauline122112 <pauline.design007@gmail.com>
2022-03-13 16:17:26 -0500

    imported firebase

--- murphy-gallery/src/comps/Title.js
+++ murphy-gallery/src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      
+      <h2>Murphy Calvert</h2>
+      <p>Welcome to my world</p>
     </div>
   )
 }
---
+++ murphy-gallery/src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBBIEcntHb9QNaq3078Ptt4BuiO3W5z4q8",
+	authDomain: "murphy-portfolio.firebaseapp.com",
+	projectId: "murphy-portfolio",
+	storageBucket: "murphy-portfolio.appspot.com",
+	messagingSenderId: "181524391616",
+	appId: "1:181524391616:web:38821b5313095f56fb6f25",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
