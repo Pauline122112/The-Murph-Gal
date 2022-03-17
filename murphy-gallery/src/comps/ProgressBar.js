858b2fe0f9ab7d7713349cb644807205220da7be
Pauline122112 <pauline.design007@gmail.com>
2022-03-17 12:43:28 -0500

    working on progress bar and running into bugs

--- murphy-gallery/src/comps/ProgressBar.js
+++ murphy-gallery/src/comps/ProgressBar.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useStorage from '../hooks/useStorage'
+import { motion } from "framer-motion"
 
-const ProgressBar = () => {
+const ProgressBar = ({ file, setFile}) => {
+  const { url, progress } = useStorage(file)
+
+  useEffect(() => {
+		if (url) {
+			setFile(null);
+		}
+	}, [url, setFile]);
+
   return (
-    <div className='progress-bar'>Progress Bar</div>
-  )
+		<motion.div
+			className="progress-bar"
+			initial={{ width: 0 }}
+			animate={{ width: progress + "%" }}
+		></motion.div>
+	);
 }
 
 export default ProgressBar
