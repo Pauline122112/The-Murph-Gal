62e1477193b1546bf15cee93bd2506c56e4159be
Pauline122112 <pauline.design007@gmail.com>
2022-03-16 21:35:11 -0500

    firebase is still unresponsive

--- murphy-gallery/src/firebase/config.js
+++ murphy-gallery/src/firebase/config.js
@@ -1,10 +1,8 @@
-import { initializeApp } from "firebase/app"
-import "firebase/storage";
-import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import 'firebase/storage'
+import 'firebase/firestore'
+import 'firebase'
 
-const projectM = process.env.FIREBASE_TOKEN;
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
 	apiKey: "AIzaSyBBIEcntHb9QNaq3078Ptt4BuiO3W5z4q8",
 	authDomain: "murphy-portfolio.firebaseapp.com",
@@ -12,15 +10,12 @@ const firebaseConfig = {
 	storageBucket: "murphy-portfolio.appspot.com",
 	messagingSenderId: "181524391616",
 	appId: "1:181524391616:web:38821b5313095f56fb6f25",
-};
-
-
-
-// Initialize Firebase
+}
 const app = initializeApp(firebaseConfig)
 
-const projectStorage = firebase.storage();
-const projectFirestore = firebase.firestore()
-const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectM, projectStorage, projectFirestore, timestamp };
+const projectStorage = firebase.storage()
+const projectFirestore = firebase.firestore()
+
+export { projectStorage, projectFirestore }
+
